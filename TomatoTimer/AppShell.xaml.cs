2751da6a67edca0058c41f6d3f5a8c57575eafa0
Joe H <4879956+MightyJoe@users.noreply.github.com>
2025-09-20 23:24:25 -0700

    Android works. Now for iOS...

---
+++ TomatoTimer/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TomatoTimer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TomatoTimer/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TomatoTimer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TomatoTimer.Pages")]
